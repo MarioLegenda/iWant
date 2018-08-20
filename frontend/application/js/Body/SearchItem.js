6e0fb77c3e78f6222b19f3148cd3403a90dd27c1
mario <likearollingstone199@gmail.com>
2018-08-20 14:07:01 +0100

    Completed listing and website overlay

---
+++ frontend/application/js/Body/SearchItem.js
@@ -0,0 +1,31 @@
+export const SearchItem = {
+    template: `<a href="" class="search-item">
+                    <div class="shop-logo wrap">
+                        <img src="/images/ebay-logo.jpg" />
+                    </div>
+                    
+                    <span class="bordered-row-separator"></span>
+                                       
+                    <div class="item-image">
+                        <img src="https://images.bonanzastatic.com/afu/images/1747/4109/99/Pussnboots_thumb155_crop.jpg" />
+                    </div>
+                    
+                    <div class="title-wrapper wrap">
+                        <h1>Short title</h1>
+                    </div>
+                    
+                    <div class="account-wrapper wrap">
+                        <h1>Account name</h1>
+                    </div>
+                    
+                    <div class="price-wrapper wrap">
+                        <h1>
+                            <span class="price-info">15.95 $</span>
+                        </h1>
+                    </div>
+                    
+                    <div class="view-item-button-wrapper wrap">
+                        <a href="">View item</a>
+                    </div>
+               </a>`
+};
