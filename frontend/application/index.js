324fe6fb75508c5a4668674d1b8d45b4dd6120b2
mario <likearollingstone199@gmail.com>
2018-08-27 14:18:41 +0100

    Simplified vue init procedure

--- frontend/application/index.js
+++ frontend/application/index.js
@@ -1,39 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
-import {Header} from "./js/Header/Header";
+import {Init} from "./js/global";
 
-import {routes} from "./js/routes";
-import {Tools} from "./js/global";
-
-Tools.registerWindowPrototypeMethods();
-Tools.registerVuePlugins(Vue, [VueRouter, Vuex]);
-
-const store = new Vuex.Store({
-    state: {
-        uniformedSearchListing: {},
-    },
-    mutations: {
-        uniformedSearchListing (state, value) {
-            this.state.uniformedSearchListing = value;
-        },
-    }
-});
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
-
-new Vue({
-    el: '#vue_app',
-    store,
-    router: router,
-    template: `<div>
-                   <Header></Header>
-                   <router-view></router-view>
-               </div>`,
-    components: {
-        Header
-    }
-});
+Init.registerWindowPrototypeMethods();
+Init.registerVuePlugins(Vue, [VueRouter, Vuex]);
+Init.createVueInstance();
--- frontend/application/js/global.js
+++ frontend/application/js/global.js
@@ -1,4 +1,10 @@
-export class Tools {
+import Vuex from "vuex";
+import VueRouter from "vue-router";
+import {routes} from "./routes";
+import Vue from "vue";
+import {Header} from "./Header/Header";
+
+export class Init {
     static registerWindowPrototypeMethods() {
         ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp'].forEach(
             function(name) {
@@ -18,5 +24,36 @@ export class Tools {
 
     static registerVuePlugins(Vue, plugins) {
         plugins.map(plugin => Vue.use(plugin));
+    }
+
+    static createVueInstance() {
+        const store = new Vuex.Store({
+            state: {
+                uniformedSearchListing: {},
+            },
+            mutations: {
+                uniformedSearchListing (state, value) {
+                    this.state.uniformedSearchListing = value;
+                },
+            }
+        });
+
+        const router = new VueRouter({
+            mode: 'history',
+            routes: routes
+        });
+
+        new Vue({
+            el: '#vue_app',
+            store,
+            router: router,
+            template: `<div>
+                   <Header></Header>
+                   <router-view></router-view>
+               </div>`,
+            components: {
+                Header
+            }
+        });
     }
 }
