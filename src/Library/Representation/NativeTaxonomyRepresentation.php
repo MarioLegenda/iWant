fa640dedb34da185c1c48088b3d6edd767262ac3
mario <likearollingstone199@gmail.com>
2018-09-07 13:14:01 +0100

    Created NativeTaxonomyRepresentation that will make accessing taxonomies easier and lazy load them

--- src/Library/Representation/NativeTaxonomyRepresentation.php
+++ src/Library/Representation/NativeTaxonomyRepresentation.php
@@ -4,14 +4,14 @@ namespace App\Library\Representation;
 
 use App\Doctrine\Entity\NativeTaxonomy;
 use App\Doctrine\Repository\NativeTaxonomyRepository;
-use App\Library\Tools\LockedMutableHashSet;
+use App\Library\Infrastructure\Helper\TypedArray;
 
 class NativeTaxonomyRepresentation
 {
     /**
-     * @var array|NativeTaxonomy[] $taxonomy
+     * @var TypedArray|iterable $taxonomies
      */
-    private $taxonomy;
+    private $taxonomies;
     /**
      * @var NativeTaxonomyRepository $nativeTaxonomyRepository
      */
@@ -24,20 +24,50 @@ class NativeTaxonomyRepresentation
         NativeTaxonomyRepository $nativeTaxonomyRepository
     ) {
         $this->nativeTaxonomyRepository = $nativeTaxonomyRepository;
-
-        $this->taxonomy = LockedMutableHashSet::create([
-            'booksMusicMovies',
-            'autopartsMechanics',
-            'homeGarden',
-            'computerMobileGames',
-            'sport',
-            'antiquesArtCollectibles',
-            'craftsHandmade',
-        ]);
+        $this->taxonomies = TypedArray::create('string', NativeTaxonomy::class);
     }
-
-    public function booksMusicMovies(): NativeTaxonomy
+    /**
+     * @param string $name
+     * @return NativeTaxonomy
+     * @throws \RuntimeException
+     */
+    public function __get(string $name): NativeTaxonomy
     {
+        $this->populateTaxonomy($name);
 
+        return $this->taxonomies[$name];
+    }
+    /**
+     * @param string $name
+     * @throws \RuntimeException
+     */
+    private function populateTaxonomy(string $name): void
+    {
+        if (!array_key_exists($name, $this->taxonomies)) {
+            $taxonomy = $this->nativeTaxonomyRepository->findOneBy([
+                'internalName' => $name,
+            ]);
+
+            if (!$taxonomy instanceof NativeTaxonomy) {
+                $message = sprintf(
+                    'Native taxonomy \'%s\' not found. Check the taxonomies internal name',
+                    $name
+                );
+
+                throw new \RuntimeException($message);
+            }
+
+            $this->taxonomies[$name] = $taxonomy;
+        }
+
+        if (!array_key_exists($name, $this->taxonomies)) {
+            $message = sprintf(
+                'Failed to lazy load a native taxonomy with name \'%s\' in %s',
+                $name,
+                get_class($this)
+            );
+
+            throw new \RuntimeException($message);
+        }
     }
 }
