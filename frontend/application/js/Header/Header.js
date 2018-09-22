ddb5a5f30f40ebff60e95df2f9efd1a79860d120
mario <likearollingstone199@gmail.com>
2018-09-22 11:04:49 +0100

    Created frontpage search box

--- frontend/application/js/Header/Header.js
+++ frontend/application/js/Header/Header.js
@@ -3,9 +3,23 @@ export const Header = {
                    <header>
                        <router-link to="/" class="logo" exact>
                            <span class="logo-part i-part">i</span>
-                           <span class="logo-part am-part">am</span>
-                           <span class="logo-part buying-part">buying</span>
+                           <span class="logo-part am-part">Would</span>
+                           <span class="logo-part buying-part">Like</span>
                        </router-link>
+                      
+                       <div class="SearchBox">
+                           <div class="SearchBox_InputBox">
+                                <input type="text" placeholder="search what would you like" />
+                           </div>
+                           
+                           <div class="SearchBox_SubmitBox">
+                                <button><i class="fas fa-chevron-right"></i></button>
+                           </div>
+                           
+                           <div class="SearchBox_AdvancedSearch">
+                               <a href="">Advanced search <i class="fas fa-search"></i></a>
+                           </div>
+                       </div>
                    </header>
                </div>`
 };
