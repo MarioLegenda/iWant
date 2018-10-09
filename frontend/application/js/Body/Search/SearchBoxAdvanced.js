74e3e839284738b04653dbc710d83dffde22099a
mario <likearollingstone199@gmail.com>
2018-10-09 17:01:54 +0200

    Added enter keydown event to advanced search

--- frontend/application/js/Body/Search/SearchBoxAdvanced.js
+++ frontend/application/js/Body/Search/SearchBoxAdvanced.js
@@ -1,6 +1,7 @@
 export const SearchBoxAdvanced = {
     data: function() {
         return {
+            enterToSearch: false,
             text: null,
             isError: false,
             activeClass: 'InputBox',
@@ -9,12 +10,14 @@ export const SearchBoxAdvanced = {
     },
     template: `<div class="SearchBoxAdvanced">
                            <div class="SearchBox_InputBox">
-                                <input @input="onInputChange" type="text" v-model="text" v-bind:class="[activeClass, errorClass]" placeholder="what would you like?" />
+                                <input @input="onInputChange" v-on:keydown.enter="submit" type="text" v-model="text" v-bind:class="[activeClass, errorClass]" placeholder="what would you like?" />
                            </div>
                            
                            <div class="SearchBox_SubmitBox">
                                 <button @click="submit"><i class="fas fa-chevron-right"></i></button>
                            </div>
+                           
+                           <p v-if="enterToSearch" class="SearchBoxAdvanced-enter-to-search">* Press Enter to search</p>
                </div>`,
     methods: {
         submit: function() {
@@ -32,6 +35,12 @@ export const SearchBoxAdvanced = {
         },
 
         onInputChange: function() {
+            if (this.text !== null && this.text !== '' && typeof this.text !== 'undefined') {
+                this.enterToSearch = true;
+            } else {
+                this.enterToSearch = false;
+            }
+
             this.$emit('on-search-term-change', this.text);
         }
     }
