fadc03e91525b7e143ff8b052429712a4aee2754
mario <likearollingstone199@gmail.com>
2018-10-28 01:01:23 +0100

    Added initial look to items

--- frontend/application/js/Body/Search/ListingComponent/Items/EbayItems.js
+++ frontend/application/js/Body/Search/ListingComponent/Items/EbayItems.js
@@ -20,7 +20,31 @@ export const EbayItems = {
     },
     template: `
             <div class="EbayItems SearchItems" id="EbayItemsId">
-                
+                <div v-for="(item, index) in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="index" class="EbayItem SearchItem">
+                    <div class="Row ImageWrapper">
+                        <img class="Image" src="/images/start_page_background.jpg" />
+                    </div>
+                    
+                    <div class="Row TitleWrapper">
+                        <p>Some title some title</p>
+                    </div>
+                    
+                    <div class="Row PriceWrapper">
+                        <p class="Price">24.58</p>
+                    </div>
+                    
+                    <div class="Row QuickLookWrapper">
+                        <button>Quick look<i class="fas fa-caret-right"></i></button>
+                    </div>
+                    
+                    <div class="Row FullDetailsWrapper">
+                        <button>Full details<i class="fas fa-caret-right"></i></button>
+                    </div>
+                    
+                    <div class="Row MarketplaceWrapper">
+                        <a href="#">View on eBay</a>
+                    </div>
+                </div>
             </div>
             `,
     props: ['classList'],
