b8f8151a67188189483060bac63d33452317dc3f
mario <likearollingstone199@gmail.com>
2018-11-04 10:29:19 +0000

    Created item translations cache repository

---
+++ src/Doctrine/Repository/ItemTranslationRepositoryCache.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Doctrine\Repository;
+
+use App\Doctrine\Entity\ItemTranslationCache;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class ItemTranslationRepositoryCache extends ServiceEntityRepository
+{
+    /**
+     * CategoryRepository constructor.
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ItemTranslationCache::class);
+    }
+    /**
+     * @param ItemTranslationCache $itemTranslationCache
+     * @return ItemTranslationCache
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function persistAndFlush(ItemTranslationCache $itemTranslationCache): ItemTranslationCache
+    {
+        $this->getEntityManager()->persist($itemTranslationCache);
+        $this->getEntityManager()->flush();
+
+        return $itemTranslationCache;
+    }
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
+    public function getManager()
+    {
+        return parent::getEntityManager();
+    }
+}
