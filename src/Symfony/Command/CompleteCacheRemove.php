5f01f549f974345117a5f80da887fff92fe0b9d9
mario <likearollingstone199@gmail.com>
2018-11-06 12:37:56 +0000

    Created complete cache remove command

--- src/Symfony/Command/CompleteCacheRemove.php
+++ src/Symfony/Command/CompleteCacheRemove.php
@@ -1,15 +1,70 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: macbook
- * Date: 06/11/2018
- * Time: 11:29
- */
 
 namespace App\Symfony\Command;
 
 
-class CompleteCacheRemove
-{
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class CompleteCacheRemove extends BaseCommand
+{
+    /**
+     * @var ManagerRegistry $doctrine
+     */
+    private $doctrine;
+    /**
+     * CompleteCacheRemove constructor.
+     * @param ManagerRegistry $doctrine
+     */
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+
+        parent::__construct();
+    }
+    /**
+     * @inheritdoc
+     */
+    public function configure()
+    {
+        $this->setName('app:complete_cache_remove');
+    }
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     * @throws \App\Symfony\Exception\HttpException
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->makeEasier($input, $output);
+
+        $conn = $this->doctrine->getConnection();
+
+        $caches = [
+            'prepared_response_cache',
+            'search_cache',
+            'item_translation_cache',
+        ];
+
+        $this->output->writeln('');
+        $this->output->writeln(sprintf(
+            '<info>Removing \'%s\' caches</info>',
+            implode(', ', $caches)
+        ));
+
+        foreach ($caches as $cache) {
+            $command = sprintf('TRUNCATE %s', $cache);
+
+            $conn->exec($command);
+        }
+
+        $this->output->writeln('');
+        $this->output->writeln(sprintf(
+            '<info>Successfully removed cache. Command successful</info>'
+        ));
+        $this->output->writeln('');
+    }
 }
