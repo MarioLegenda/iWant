fbf294f974bb13fb8bc9d35b834875e4d3cf2d17
mario <likearollingstone199@gmail.com>
2018-11-13 15:52:00 +0000

    Removed typed js and all unneccessary features from homepage

--- frontend/application/js/Body/Homepage/Homepage.js
+++ frontend/application/js/Body/Homepage/Homepage.js
@@ -1,17 +1,7 @@
 export const Homepage = {
     template: `<div>
                     <div id="StartPage">
-                        
-                        <div class="StartPage-background-image">
-                            <img src="/images/start_page_background.jpg"/>
-                        </div>
-                   
                          <div class="StartPage_MissionStatement">
-                             <h1 class="MissionStatement-header">Our mission</h1>
-                             <div class="MissionStatement_Content">
-                                 <span class="Content-full-element-span">To provide you with a unique shopping experience with products from </span>
-                                 <span class="Content-full-element-span" id="typed_js_sentence"></span> online marketplace(s). <a href="" class="MissionStatement_Link">Read more</a> about it.
-                             </div>
                          </div>
                      </div>
                </div>`,
