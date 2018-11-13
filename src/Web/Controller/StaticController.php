8c9bd66c2c88d9363802aeedaabdb879165a608d
mario <likearollingstone199@gmail.com>
2018-11-13 15:55:04 +0000

    Removed promise template and removed redundant base template

--- src/Web/Controller/StaticController.php
+++ src/Web/Controller/StaticController.php
@@ -26,26 +26,12 @@ class StaticController
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
      */
-    public function getPromise(\Twig_Environment $templating): Response
-    {
-        $content = $templating->render('application/static/promise.html.twig');
-
-        return new Response($content);
-    }
-    /**
-     * @param \Twig_Environment $templating
-     * @return Response
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
     public function getSingleItem(\Twig_Environment $templating): Response
     {
         $content = $templating->render('application/static/single_item.html.twig');
 
         return new Response($content);
     }
-
     /**
      * @param \Twig_Environment $templating
      * @return Response
