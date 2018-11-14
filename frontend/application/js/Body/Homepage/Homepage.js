bb9f41727a331ce7833300a0f90789382f5ca630
mario <likearollingstone199@gmail.com>
2018-11-14 12:48:05 +0000

    Started working on the homepage

--- frontend/application/js/Body/Homepage/Homepage.js
+++ frontend/application/js/Body/Homepage/Homepage.js
@@ -1,8 +1,21 @@
 export const Homepage = {
     template: `<div>
                     <div id="StartPage">
-                         <div class="StartPage_MissionStatement">
-                         </div>
-                     </div>
+                        <div class="Row">
+                            <p class="Advert">One website</p>
+                        </div>
+                        
+                        <div class="Row">
+                            <p class="Advert">Entire world of eBay</p>
+                        </div>
+                        
+                        <div class="Row">
+                            <p class="Advert">Translated to Your language</p>
+                        </div>
+                        
+                        <div class="Row">
+                            <p class="Advert">Whatever you would like</p>
+                        </div>
+                    </div>
                </div>`,
 };
