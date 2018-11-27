91484630bcf43330483c6c50efdd2244639426f1
mario <likearollingstone199@gmail.com>
2018-11-27 17:43:42 +0000

    Made the hide duplicates explanation popup be a click event

--- frontend/application/js/Body/Search/Filters/Choosing/HideDuplicateItems.js
+++ frontend/application/js/Body/Search/Filters/Choosing/HideDuplicateItems.js
@@ -1,15 +1,29 @@
 export const HideDuplicateItems = {
+    data: function() {
+        return {
+            isToggleOpen: false,
+        }
+    },
     template: `
         <div class="SingleFilterWrapper">
             <toggle-button
                 id="changed-font"
-                :sync="true" 
-                @change="onChange" 
+                :sync="true"
+                @change="onChange"
                 :labels="{checked: translationsMap.filters.hideDuplicateItemsHide, unchecked: translationsMap.filters.hideDuplicateItemsShow}"
                 :width="180"
                 :height="40"></toggle-button>
                 
-            <div class="fas fa-question TooltipContent" v-tooltip="translationsMap.filters.hideDuplicateItemsExplanation"></div>
+                <v-popover>
+                    <div class="fas fa-question TooltipContentButton"></div>
+                    
+                    <template slot="popover">
+                        <div class="TooltipContent">
+                            {{translationsMap.filters.hideDuplicateItemsExplanation}}
+                        </div>
+                        
+                    </template>
+                </v-popover>
         </div>
     `,
     computed: {
@@ -18,6 +32,9 @@ export const HideDuplicateItems = {
         },
     },
     methods: {
+        openInformation: function() {
+            this.isToggleOpen = !this.isToggleOpen;
+        },
         onChange: function(event) {
             this.$store.commit('filtersEvent', {
                 hideDuplicateItems: event.value,
